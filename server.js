a5a0009f037a31e5519fb85891b657d44e18c866
Sam Mailand <sam.mailand@gmail.com>
2015-11-05 08:46:49 -0500

    Move file hierarchy for CSS
    The file hierarchy has been changed to follow better convension (and for CSS use)

--- server.js
+++ server.js
@@ -4,9 +4,12 @@ var path = require('path');
 var app = express();
 var port = process.env.PORT || 3000;
 
+app.use(express.static(path.join(__dirname, '/public/CSS')));
+
 app.use('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '/index.html'));
+  res.sendFile(path.join(__dirname, '/views/index.html'));
 });
+
 
 app.listen(port, function() {
   console.log('App is listening on port ' + port);
